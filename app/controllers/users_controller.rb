866d65fdf570204a4ba96650029fc43d3cf508df
John L <johnlamyc@gmail.com>
2017-07-04 14:52:47 -0700

    Add User#Create and style changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,29 @@ class UsersController < ApplicationController
   def show
     @bids = Bid.where(user: current_user).order(created_at: :desc)
   end
+
+  def new
+  @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path, notice: 'Account Created Successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+    :name,
+    :email,
+    :password,
+    :password_confirmation
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :bids, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:new, :create, :index, :show] do
 
   end
 
